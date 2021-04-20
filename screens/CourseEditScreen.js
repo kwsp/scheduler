dea737b333db28fa285d65dbb4ed3c95aa1b5d60
Tiger Nie <nhl0819@gmail.com>
2021-04-20 18:19:26 +0800

    added formik form + yup validation = edit screen

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -1,24 +1,60 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import Form from "../components/Form";
+import * as Yup from "yup";
 
-const Field = ({ label, value }) => {
-  return (
-    <View style={styles.fieldContainer}>
-      <Text style={styles.label}>{label}</Text>
-      <Text style={styles.field}>{value}</Text>
-    </View>
-  );
-};
+const validationSchema = Yup.object().shape({
+  id: Yup.string()
+    .required()
+    .matches(/(F|W|S)\d{3,}/, "Must be a term and 3-digit number")
+    .label("ID"),
+  meets: Yup.string()
+    .required()
+    .matches(
+      /(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/,
+      "Must be weekdays followed by start and end time"
+    )
+    .label("Meeting times"),
+  title: Yup.string().required().label("Title"),
+});
 
 const CourseEditScreen = ({ navigation, route }) => {
   const course = route.params.course;
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Field label="ID" value={course.id} />
-        <Field label="Meeting times" value={course.meets} />
-        <Field label="Title" value={course.title} />
+        <Form
+          initialValues={{
+            id: course.id,
+            meets: course.meets,
+            title: course.title,
+          }}
+          validationSchema={validationSchema}
+        >
+          <Form.Field
+            name="id"
+            leftIcon="identifier"
+            placeholder="F110"
+            autoCapitalize="none"
+            autoFocus={true}
+            style={styles.field}
+          />
+          <Form.Field
+            name="meets"
+            leftIcon="calendar-range"
+            placeholder="MThu 12:00-13:50"
+            autoCapitalize="none"
+            style={styles.field}
+          />
+          <Form.Field
+            name="title"
+            leftIcon="format-title"
+            placeholder="Introduction to programming"
+            style={styles.field}
+          />
+        </Form>
       </ScrollView>
     </SafeAreaView>
   );
@@ -32,10 +68,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#ccccb3",
   },
   field: {
-    height: 40,
     width: 300,
     padding: 5,
-    backgroundColor: "white",
   },
   fieldContainer: {
     marginTop: 10,
